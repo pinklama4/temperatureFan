df8aaf99dbadc1f81277052688810500aa8330e2
Новокрещенов Артем <102524049+pinklama4@users.noreply.github.com>
2024-03-04 20:37:07 +0300

    start progect: v-0001

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 
-let temperature = 0
+let temperature = [36.5, 36.6, 36.4, 36.3, 37.5, 37.7, 37.8, 39.0, 39.5, 38.8]
 const button = document.querySelector('.btn')
 
 
 function sayTemperature() {
-    let message  = 'Ваша температура: ' + temperature
+    let randomItem = temperature[Math.floor(Math.random() * temperature.length)];
+    let message  = 'Ваша температура: ' + randomItem
     alert(message);
 }
 
