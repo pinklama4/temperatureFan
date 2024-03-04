e0d9ffe25e26f4844b9899d0603f61c988e59cb7
Новокрещенов Артем <102524049+pinklama4@users.noreply.github.com>
2024-03-04 20:54:03 +0300

    add: time on function

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 
 let temperature = [36.5, 36.6, 36.4, 36.3, 37.5, 37.7, 37.8, 39.0, 39.5, 38.8]
 const button = document.querySelector('.btn')
+const printTemperature = document.querySelector('.print-temp')
 
 
 function sayTemperature() {
     let randomItem = temperature[Math.floor(Math.random() * temperature.length)];
     let message  = 'Ваша температура: ' + randomItem
-    alert(message);
+    // alert(message);
+    printTemperature.innerHTML = randomItem;
 }
 
 
 button.addEventListener('click', function () {
-    sayTemperature();
+    setTimeout(sayTemperature, 3000);
 })
 
